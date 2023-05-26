81709d53238f440438b08a81fd9fdc1cf28e85c1
webstoryboy <webstupids@gmail.com>
2023-05-26 10:37:01 +0900

    2023.05.26

--- w_webd/site/site5/script/script.js
+++ w_webd/site/site5/script/script.js
@@ -1,16 +1,16 @@
 $(function(){
     // 이미지 슬라이드
-    let currentIndex = 0;   
-    $(".sliderWrap").append($(".slider").first().clone(true)); 
+    let currentIndex = 0;
+    $(".sliderWrap").append($(".slider").first().clone(true));
 
-    setInterval(function(){   
-        currentIndex++;    
-        $(".sliderWrap").animate({marginTop: -currentIndex * 350 + "px"}, 600);   
+    setInterval(function(){
+        currentIndex++;
+        $(".sliderWrap").animate({marginLeft: -100 * currentIndex + "%" }, 600);
 
-        if(currentIndex == 3){  
+        if(currentIndex == 3){
             setTimeout(function(){
-                $(".sliderWrap").animate({ marginTop:0 }, 0);   
-                currentIndex = 0;  
+                $(".sliderWrap").animate({marginLeft: 0}, 0);
+                currentIndex = 0;
             }, 700);
         }
     }, 3000);
@@ -23,11 +23,11 @@ $(function(){
         $(this).find(".submenu").stop().slideUp();
     });
 
-    // 팝업 
+    // 팝업
     $(".popup-btn").click(function(){
         $(".popup-view").show();
     });
     $(".popup-close").click(function(){
-        $(".popup-view").hide();
+        $(".popup-view").hide ();
     });
 });
