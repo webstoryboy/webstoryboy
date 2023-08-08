1ff3dfc8e23695f1663dfc4e0e8c52d2b33a6c4e
webstoryboy <webstupids@gmail.com>
2023-08-08 09:28:53 +0900

    2023.08.08

--- c_react/react_youtube/src/components/Videos.jsx
+++ c_react/react_youtube/src/components/Videos.jsx
@@ -6,7 +6,7 @@ const Videos = ({ videos, layout }) => {
   if (!videos?.length) return <Loader />
 
   return (
-    <article className={`videos__inner ${layout}`} >
+    <article className={`videos__inner ${layout}`}>
       {videos.map((item, idx) => (
         <VideoCard key={idx} video={item} />
       ))}
